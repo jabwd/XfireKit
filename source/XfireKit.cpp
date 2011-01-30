24f7075f32068985c20768616a7ac426c3673e48
Oliver Ney <oliver@dryder.de>
2011-01-31 00:49:52 +0100

    Modified logging mechanism and added XfireKit namespace to XfireKit.cpp

--- source/XfireKit.cpp
+++ source/XfireKit.cpp
@@ -1,55 +1,41 @@
 #include "XfireKit.h"
 
- /*
-  * printf() but then disabled when debug logging is turned off
-  */
-void xfPrintf(const char *format,...)
+namespace XfireKit
 {
-	#if DEBUG_LOGGING
-	fprintf(stderr, "  XfireKit: ");
-	va_list args;
-	va_start(args, format);
-	vfprintf(stderr, format, args);
-	va_end(args);
-	fprintf(stderr, "\n");
-	fflush(stderr);
-	#endif
-} // xfPrintf();
+	uint32 ipFromString(const char *ip)
+	{
+		return 0; // TBD
+	}
+
+	const char *stringFromIP(uint32 ip)
+	{
+		return NULL;
+	}
 
 
-XfireKit::uint32 ipFromString(const char *ip)
-{
-	return 0; // TBD
+	/*
+	 * Byte swapping, it swaps Little to Big and Big to little
+	 */
+	uint16 endianSwap(uint16 x)
+	{
+		return (x>>8)|(x<<8);
+	}
+
+	uint32 endianSwap(uint32 x)
+	{
+		return (x>>24)|((x<<8) & 0x00FF0000)|((x>>8) & 0x0000FF00)|(x<<24);
+	}
+
+	uint64 endianSwap(uint64 x)
+	{
+		return	(x>>56)	|
+				((x<<40) & 0x00FF000000000000)	|
+				((x<<24) & 0x0000FF0000000000)	|
+				((x<<8)  & 0x000000FF00000000)	|
+				((x>>8)  & 0x00000000FF000000)	|
+				((x>>24) & 0x0000000000FF0000)	|
+				((x>>40) & 0x000000000000FF00)	|
+				(x<<56);
+	}
+	// byte swapping
 }
-
-const char *stringFromIP(XfireKit::uint32 ip)
-{
-	return NULL;
-}
-
-
-/*
- * Byte swapping, it swaps Little to Big and Big to little
- */
-XfireKit::uint16 endianSwap(XfireKit::uint16 x)
-{
-	return (x>>8)|(x<<8);
-}
-
-XfireKit::uint32 endianSwap(XfireKit::uint32 x)
-{
-	return (x>>24)|((x<<8) & 0x00FF0000)|((x>>8) & 0x0000FF00)|(x<<24);
-}
-
-XfireKit::uint64 endianSwap(XfireKit::uint64 x)
-{
-	return	(x>>56)									| 
-					((x<<40) & 0x00FF000000000000)	|
-					((x<<24) & 0x0000FF0000000000)	|
-					((x<<8)  & 0x000000FF00000000)	|
-					((x>>8)  & 0x00000000FF000000)	|
-					((x>>24) & 0x0000000000FF0000)	|
-					((x>>40) & 0x000000000000FF00)	|
-					(x<<56);
-}
-// byte swapping
